b8fb4af68b0c1680be2df253e56f7a5833962b98
umidsamadov0927 <umidsamadov0927@gmail.com>
2025-09-02 18:48:58 +0500

    addd

--- src/pages/Faq.vue
+++ src/pages/Faq.vue
@@ -1,24 +1,25 @@
+//chatgpt 70% yordam berdi ulgirmadim 45% uzm yozdim
 <template>
-  <div v-for="item in data" :key="item.id" class="flex items-center justify-center">
-
+  <div v-for="(item, index) in data" :key="index" class="flex flex-col items-center">
+    <div @click="open(index)" class="cursor-pointer bg-gray-200 px-3 py-1 rounded">
+      {{ item.name }}
+    </div>
+    <p v-if="openedIndex === index">{{ item.email }}</p>
   </div>
 </template>
+
 <script setup>
+import { ref } from "vue";
+
 let data = [
-  {
-    name: "John",
-    email: "john@gmail.com",
-    password: "123456",
-  },
-  {
-    name: "Johan",
-    email: "john@gmail.com",
-    password: "123456",
-  },
-  {
-    name: "Johan-Medium",
-    email: "john@gmail.com",
-    password: "123456",
-  }
-]
+  { name: "John", email: "john@gmail.com", password: "123456" },
+  { name: "Johan", email: "johan@gmail.com", password: "123456" },
+  { name: "Johan-Medium", email: "medium@gmail.com", password: "123456" }
+];
+
+let openedIndex = ref(null);
+
+function open(index)
+  openedIndex.value = openedIndex.value === index ? null : index;
+}
 </script>
